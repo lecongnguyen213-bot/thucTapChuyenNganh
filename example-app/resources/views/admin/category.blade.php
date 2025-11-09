4934b3c49ddafaa9ddd3ca94b91cee74a0bbe31f
nguyen <lecongnguyen213@gmail.com>
2025-11-09 14:05:02 +0700

    Hoan thanh bao cao tuan 4

---
+++ example-app/resources/views/admin/category.blade.php
@@ -0,0 +1,5 @@
+@extends('layout/admin')
+@section('body')
+    <h1>Product List Page</h1>
+    <h2>Em chua tim duoc layout nao ung y'</h2>
+@endsection
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -12,3 +12,12 @@ Route::get('/cart', function () {
 Route::get('/about', function () {
     return view('about');
 })->name('about');
+Route::get('/admin', function () {
+    return view('admin');
+})->name('admin');
+Route::get('/admin/product-list', function () {
+    return view('admin/product-list');
+})->name('product-list');
+Route::get('/admin/category', function () {
+    return view('admin/category');
+})->name('category');
